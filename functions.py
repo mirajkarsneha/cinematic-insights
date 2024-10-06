7e5e70f0a477abee04bccc8223edc34d4e70537d
Sneha Mirajkar <sneha.miraj009@gmail.com>
2024-10-06 13:25:18 +0200

    Create main and function file and move hypothesis code in it

--- functions.py
+++ functions.py
@@ -1,3 +1,61 @@
+import string
+import numpy as np
+import pandas as pd
+import re
+
+# Function to check if a title contains only numbers or special characters (no alphabets)
+def is_only_numbers_or_special_chars(title):
+    return bool(re.match(r'^[^a-zA-Z]+$', title))
+
+
+# Function to standardize documentary genres
+def standardize_documentry_genres(genre):
+    if isinstance(genre, str):
+        if 'Documentary' in genre:
+            return 'Documentary'
+    return genre
+
+# Function to standardize animation genres
+def standardize_animation_genres(genre):
+    if isinstance(genre, str):
+        # Replace specific animation genres with 'Animation'
+        if 'Animation' in genre:
+            return 'Animation'
+    return genre
+    
+# Function to standardize animation genres
+def standardize_biography_genres(genre):
+    if isinstance(genre, str):
+        # Replace specific animation genres with 'Animation'
+        if 'Biography' in genre:
+            return 'Biography'
+    return genre
+
+# Function to standardize animation genres
+def standardize_adventure_genres(genre):
+    if isinstance(genre, str):
+        # Replace specific animation genres with 'Animation'
+        if 'Adventure' in genre:
+            return 'Adventure'
+    return genre
+
+# Function to check if a title contains only numbers or special characters (no alphabets)
+def is_only_numbers_or_special_chars(title):
+    # Regex to check if the title does NOT contain any alphabetic characters (only numbers and special characters)
+    return bool(re.match(r'^[^a-zA-Z]+$', title))
+
+# Function to extract runtime in minutes from the string
+def extract_runtime(runtime_str):
+    if isinstance(runtime_str, str):
+        # Extract the numeric part of the runtime
+        return int(runtime_str.split()[0])  # Convert to int
+    return None
+
+# Function to classify by runtime
+def classify_by_runtime(runtime):
+    if pd.isna(runtime):
+        return 'Unknown'
+        
 {
  "cells": [
   {
